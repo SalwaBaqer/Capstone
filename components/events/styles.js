673476e460cf2ead9c0738bd9dddf4551b3490cc
SalwaBaqer <s.baqer@hotmail.com>
2021-01-08 16:21:43 +0300

    few updates

--- components/events/styles.js
+++ components/events/styles.js
@@ -1,16 +1,9 @@
 import styled from "styled-components/native";
 
-import { Icon } from "native-base";
-
 export const ItemWrapper = styled.View`
   margin-top: 2%;
   align-items: center;
   justify-content: center;
   background-color: #e6efff;
   width: 90%;
-`;
-
-export const AddButtonStyled = styled(Icon)`
-  color: #e6efff;
-  margin-right: 10px;
 `;
