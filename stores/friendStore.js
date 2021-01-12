7ec908914a0fb14a3d41a2df4aa79a167399d7d8
SalwaBaqer <s.baqer@hotmail.com>
2021-01-12 19:41:59 +0300

    friend store

--- stores/friendStore.js
+++ stores/friendStore.js
@@ -4,9 +4,6 @@ import { makeAutoObservable } from "mobx";
 //instance
 import instance from "./instance";
 
-//store
-import authStore from "./authStore";
-
 class FriendStore {
   friends = [];
 
@@ -14,7 +11,7 @@ class FriendStore {
     makeAutoObservable(this);
   } //end constructor
 
-  //add friend
+  //send friend request
   SendFriendReq = async (user2Id) => {
     try {
       const response = await instance.post(`/friend/sendRequest/${user2Id}`);
@@ -22,7 +19,49 @@ class FriendStore {
     } catch (error) {
       console.error("friendStore --> SendFriendReq", error);
     }
-  }; //end add friend
+  }; //end send friend request
+
+  //withdraw friend request
+  WithdrawFriendReq = async (user2Id) => {
+    try {
+      const response = await instance.put(`/friend/withdrawRequest/${user2Id}`);
+      this.friends.push(response.data);
+    } catch (error) {
+      console.error("friendStore --> SendFriendReq", error);
+    }
+  }; //end withdraw friend request
+
+  //accept incoming requests
+  AcceptFriendReq = async () => {
+    try {
+    } catch (error) {
+      console.error("friendStore --> AcceptFriendReq", error);
+    }
+  }; //end accept incoming requests
+
+  //decline incoming requests
+  DeclineFriendReq = async () => {
+    try {
+    } catch (error) {
+      console.error("friendStore --> DeclineFriendReq", error);
+    }
+  }; //end decline incoming requests
+
+  //delete firend
+  DeleteFriend = async () => {
+    try {
+    } catch (error) {
+      console.error("friendStore --> DeleteFriend", error);
+    }
+  }; //end delete firend
+
+  //block user
+  BlockUser = async () => {
+    try {
+    } catch (error) {
+      console.error("friendStore --> BlockUser", error);
+    }
+  }; //end block user
 } //end class
 
 const friendStore = new FriendStore();
