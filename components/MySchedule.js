0e3a1c930c1f4d41ad11e83a3d51fafd3eaf60fd
kalmansour <khaledalmansour24@gmail.com>
2021-01-13 10:22:25 +0300

    schedule wip

--- components/MySchedule.js
+++ components/MySchedule.js
@@ -1,12 +1,22 @@
 //Libraries
 import React, { useState } from "react";
-import { View, TouchableOpacity } from "react-native";
 import { Agenda } from "react-native-calendars";
-import { Card, Avatar } from "react-native-paper";
+import { Card } from "react-native-paper";
 import { Text } from "native-base";
+import { observer } from "mobx-react";
 
 //Stores
 import authStore from "../stores/authStore";
+import eventStore from "../stores/eventStore";
+
+//styles
+import {
+  AgendaStyled,
+  RednerItemButton,
+  RenderItemStyled,
+  RenderItemImageStyled,
+  RenderEmptyDateStyled,
+} from "../styles";
 
 const timeToString = (time) => {
   const date = new Date(time);
@@ -16,17 +26,28 @@ const timeToString = (time) => {
 const MySchedule = () => {
   const [items, setItems] = useState({});
 
+  //Just signed in user events display
   const loadItems = (day) => {
+    const profileEvents = eventStore.events.filter(
+      (event) => event.userId === authStore.user.id
+    );
+    const events = profileEvents.filter(
+      (event) => event.date.split("T")[0] === day.dateString
+    );
+    console.log("day", day);
+    console.log(" day datestring", day.dateString);
+
     setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+      for (let i = 0; i < events.length; i++) {
+        const time = day.timestamp;
         const strTime = timeToString(time);
         if (!items[strTime]) {
           items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
+          for (let j = 0; j < events.length; j++) {
             items[strTime].push({
-              name: "Item for " + strTime + " #" + j,
+              name: events[j].name,
+              label: events[j].label,
+              image: events[j].image,
               height: Math.max(50, Math.floor(Math.random() * 150)),
             });
           }
@@ -42,27 +63,31 @@ const MySchedule = () => {
 
   const renderItem = (item) => {
     return (
-      <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
+      <RednerItemButton>
         <Card>
           <Card.Content>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
+            <RenderItemStyled>
               <Text>{item.name}</Text>
-              <Avatar.Image source={{ uri: authStore.user.image }} />
-            </View>
+              <Text>{item.label}</Text>
+              <RenderItemImageStyled source={{ uri: item.image }} />
+              <Text>{authStore.user.username}</Text>
+            </RenderItemStyled>
           </Card.Content>
         </Card>
-      </TouchableOpacity>
+      </RednerItemButton>
+    );
+  };
+
+  const renderEmptyDate = () => {
+    return (
+      <RenderEmptyDateStyled>
+        <Text>This is an empty date!</Text>
+      </RenderEmptyDateStyled>
     );
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <AgendaStyled>
       <Agenda
         items={items}
         loadItemsForMonth={loadItems}
@@ -71,9 +96,10 @@ const MySchedule = () => {
         minDate={"2021-01-01"}
         maxDate={"2021-12-31"}
         renderItem={renderItem}
+        renderEmptyDate={renderEmptyDate}
       />
-    </View>
+    </AgendaStyled>
   );
 };
 
-export default MySchedule;
+export default observer(MySchedule);
